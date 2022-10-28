10a1575e2ebd1787d3de0841e306c9d578bfad36
HarshitS45 <20bcs054@iiitdwd.ac.in>
2022-10-28 16:19:19 +0530

    Template Queue

--- Queue.cpp
+++ Queue.cpp
@@ -4,17 +4,18 @@
  * Available operations - 
  * push(T element) - Add an element at the end of the queue
  * T pop() - Pop an element from the front of the queue. Throws exception when no element in the queue
- * T check() - Returns element at the front of the queue without popping it out of the queue. Returns 0 if not available
+ * T check() - Returns element at the front of the queue ithout popping it out of the queue. Returns 0 if not available
  * int size() - Returns the size of the queue
+ * void print() - Print the elements present in the queue.
  */
 
 #include<iostream>
 
 using namespace std;
 
-struct Node {
+template <typename T> struct Node {
         private:
-                int data;
+                T data;
                 Node* next;
 
 	public:
@@ -22,7 +23,7 @@ struct Node {
                         this->next = 0;
                 }
 
-                Node(int data) {
+                Node(T  data) {
                         this->data = data;
                         this->next = 0;
                 }
@@ -31,7 +32,7 @@ struct Node {
                         return this->next != 0;
                 }
 
-                int getData() {
+                T getData() {
                         return this->data;
                 }
 
@@ -44,69 +45,65 @@ struct Node {
                 }
 };
 
-class Queue{
+template <typename T> class Queue{
 	private:
-		Node *head=0;
+		Node<T> *head=0;
 	public:
-		void push(int element);
-		void pop();
-		int check();
-		int size();
-		void print();
-};
+		void push(T element){
+			 Node<T> *node = new Node(element);
+       			 if(head == 0) {
+               			 head = node;
+               			 return;
+   			 }   
+       			 Node<T> *temp = head;
+        		 while(temp->hasNext()) {
+                		 temp = temp->getNext();
+       			 }   
+    			 temp->setNext(node);
+		}
 
-void Queue::push(int element){
-	Node *node = new Node(element);
-        if(head == 0) {
-        	head = node;
-               	return;
-        }
-        Node *temp = head;
-        while(temp->hasNext()) {
-       		temp = temp->getNext();
-        }
-        temp->setNext(node);
-};
+		void pop(){
+			if(head==0){
+				cout<<"No items in Queue\n";
+				return;
+			}
+		        Node<T> *temp=head;
+ 		        head=temp->getNext();
+        		temp->setNext(0);
+		}
 
-void Queue::pop(){
-	Node* temp=head;
-	head=temp->getNext();
-	temp->setNext(0);
-	return;
-};
+		T check(){
+			return head->getData();
+		}
 
-int Queue::size(){
-	Node* temp=head;
-	int count=0;
-	while(temp->hasNext()){
-		temp=temp->getNext();
-		count++;
-	}
-	count++;
-	return count;
-};
+		int size(){
+		        Node<T> *temp=head;
+       		        int count=0;
+        		while(temp->hasNext()){
+                		temp=temp->getNext();
+                		count++;
+        		}
+        		count++;
+        		return count;
+		}
 
-int Queue::check(){
-	return head->getData();
+		void print(){
+		        if(head == 0) {
+                		cout<<"No items in the list\n";
+                		return;
+        		}
+        		Node<T> *temp = head;
+        		while(temp->hasNext()) {
+                		cout<<temp->getData()<<"\t";
+                		temp = temp->getNext();
+        		}
+        		cout<<temp->getData()<<"\t";
+        		cout<<endl;
+		}
 };
-
-void Queue::print() {
-        if(head == 0) {
-        	cout<<"No items in the list\n";
-     	   	return;
-	}
-	Node* temp = head;
-        while(temp->hasNext()) {
-        	cout<<temp->getData()<<"\t";
-       		temp = temp->getNext();
-        }
-       	cout<<temp->getData()<<"\t";
-	cout<<endl;
-};
-
 
 int main(){
-	Queue A;
+	Queue<int> A;
 	A.push(1);
 	A.push(2);
 	A.print();
@@ -114,13 +111,17 @@ int main(){
 	A.pop();
 	A.push(4);
 	A.print();
-	A.pop();
-	A.push(5);
-	A.print();
 	int x=A.size();
 	cout<<"Size of the Queue is = "<<x<<endl;
 	int y=A.check();
 	cout<<"Top element is = "<<y<<endl;
+	Queue<string> B;
+	B.push("A");
+	B.push("B");
+	B.pop();
+	B.pop();
+	B.pop();
+	B.print();
 }
 
 
